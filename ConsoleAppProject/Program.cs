53b548b0a5b4d920c0a6387aac0c7b48e1cd3663
Phill Horrocks <eighteentee@users.noreply.github.com>
2021-01-22 12:41:38 +0000

    Added converter to the program class

--- ConsoleAppProject/Program.cs
+++ ConsoleAppProject/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ConsoleAppProject.App01;
+using System;
 
 namespace ConsoleAppProject
 {
@@ -20,7 +21,8 @@ namespace ConsoleAppProject
             Console.WriteLine();
             Console.Beep();
 
-
+            DistanceConverter converter = new DistanceConverter();
+            converter.Run();
         }
     }
 }
