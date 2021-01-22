26321ebfad3b98245c8bf7b2cb2860650aff3fc9
Phill Horrocks <eighteentee@users.noreply.github.com>
2021-01-22 12:27:14 +0000

    Update attributes and methods

--- ConsoleAppProject/App01/DistanceConverter.cs
+++ ConsoleAppProject/App01/DistanceConverter.cs
@@ -1,13 +1,49 @@
 ﻿namespace ConsoleAppProject.App01
 {
     /// <summary>
-    /// Please describe the main features of this App
+    /// This is a console app to convert distance from one unit to another
     /// </summary>
     /// <author>
-    /// Student Name version 0.1
+    /// Phill Horrocks version 0.1
     /// </author>
     public class DistanceConverter
     {
+
+        private double miles;
+        private double feet;
+
+        /// <summary>
+        /// Main run method
+        /// </summary>
+        public void Run()
+        {
+
+        }
+
+        /// <summary>
+        /// Ask the user to input the miles
+        /// Input miles as a double
+        /// </summary>
+        private void InputMiles()
+        {
+
+        }
+
+        /// <summary>
+        /// Calculate the feet from the miles inputted
+        /// </summary>
+        private void CalculateFeet()
+        {
+
+        }
+
+        /// <summary>
+        /// Output the conversion to the user
+        /// </summary>
+        private void OutputFeet()
+        {
+
+        }
 
     }
 }
