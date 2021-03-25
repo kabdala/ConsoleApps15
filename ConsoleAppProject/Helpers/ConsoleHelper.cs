1f328f843b72549fbfcb5d5beb59124511e5fb61
Phill Horrocks <eighteentee@users.noreply.github.com>
2021-03-25 16:00:09 +0000

    Update comment in ConsoleHelper

--- ConsoleAppProject/Helpers/ConsoleHelper.cs
+++ ConsoleAppProject/Helpers/ConsoleHelper.cs
@@ -117,6 +117,11 @@ namespace ConsoleAppProject.Helpers
         /// and the name of the author and a prompt to
         /// inform the use which units are being converted
         /// Please change the authors name.
+        /// 
+        /// EDIT By Phill Horrocks
+        /// I also included the ability to insert a version number
+        /// into the heading to ensure it is displayed on every
+        /// app using this helper.
         /// </summary>
         public static void OutputHeading(string heading, double version)
         {
